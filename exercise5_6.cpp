704d4e807efec113272ccc0fc9694399eed6e4a3
bed-com-27-23 <bed-com-27-23@unima.ac.mw>
2025-05-19 17:42:16 +0200

    Update exercise5_6.cpp

--- exercise5_6.cpp
+++ exercise5_6.cpp
@@ -53,7 +53,7 @@ int vowelsCount(const string vow)
 int wordsCount(const string& wed){
      int count = 0;
      for (int i = 0; i < wed.size(); i++){        
-         if (isspace(wed[i])){
+         if ((wed[i] = ' ')){
                count++;
          }
      }
